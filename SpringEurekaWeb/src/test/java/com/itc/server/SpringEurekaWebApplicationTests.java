fa0f194bedc927abc0942db5fbb134153e28f1c7
tutan <tutan.essential@gmail.com>
2021-07-25 16:20:52 +0000

    first commit

---
+++ SpringEurekaWeb/src/test/java/com/itc/server/SpringEurekaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
